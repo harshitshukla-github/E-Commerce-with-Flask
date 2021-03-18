f975827e2a972b2c2307f9be22f7d3b4a2c3d87a
HARSHIT SHUKLA <erharshitshukla100.hs@gmail.com>
2021-03-18 10:26:32 +0530

    After adding db

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from market import app
 
-@app.route('/')
-@app.route('/home')
-def home_page():
-  return render_template('home.html')
-
-@app.route('/market')
-def market_page():
-  items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
-  return render_template('market.html', items = items)
-
+#Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
