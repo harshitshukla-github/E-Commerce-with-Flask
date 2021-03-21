3a73231254b7c51e102c3c980b15306045873069
HARSHIT SHUKLA <erharshitshukla100.hs@gmail.com>
2021-03-21 14:12:52 +0530

    After completing form

---
+++ market/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from market.models import User
+
+class RegisterForm(FlaskForm):
+
+  def validate_username(self, username_to_check):
+      user = User.query.filter_by(username = username_to_check.data).first()
+      if user:
+        raise ValidationError('Username already exists!, Please try a different username')
+
+  def validate_email_address(self, email_address_to_check):
+      email_address = User.query.filter_by(email_address = email_address_to_check.data).first()
+      if email_address:
+        raise ValidationError('Email Address already exist! Please try a different email address.')
+
+  username = StringField(label= 'User Name:', validators= [Length(min=2, max=30), DataRequired()])
+  email_address = StringField(label='Email Address:', validators = [Email(), DataRequired()])
+  password1 = PasswordField(label='Password:', validators= [Length(min=6), DataRequired()])
+  password2 = PasswordField(label='Confirm Password:', validators = [EqualTo('password1'), DataRequired()])
+  submit = SubmitField(label='Create Account')
