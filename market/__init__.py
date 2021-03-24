d01c09074dd1134e326e513f40e7f837794052f5
HARSHIT SHUKLA <erharshitshukla100.hs@gmail.com>
2021-03-24 08:00:53 +0530

    Item Purchasing Part 1

--- market/__init__.py
+++ market/__init__.py
@@ -2,11 +2,15 @@ import bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '0339f475c8af0fa86a7a6bc0'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,3 +1,5 @@
+from flask_login import login_user, logout_user, login_required
+from sqlalchemy.orm import query
 from market.forms import LoginForm, RegisterForm
 from market import app
 from flask import render_template, redirect, url_for, flash
@@ -10,6 +12,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -23,6 +26,9 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
+
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
@@ -32,4 +38,21 @@ def register_page():
 @app.route('/login', methods = ['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username = form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password= form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as:{attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash(f'Username and password does not match! Please try again.', category='danger')
     return render_template('login.html', form = form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been logged out!!!', category='info')
+    return redirect(url_for("home_page"))
+
+
